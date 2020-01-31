f002f599dc3906a757d0d252a505102d211ec6c9
Anthony Spears <aspears@gatech.edu>
2020-01-31 16:55:14 -0500

    compiles and runs gtsam now but there is a bug

--- PressureFactor/PressureFactor.cpp
+++ PressureFactor/PressureFactor.cpp
@@ -25,12 +25,17 @@
 
 // We will use Pose2 variables (x, y, theta) to represent the robot positions
 #include <gtsam/geometry/Pose2.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/Rot3.h>
+
+
 
 // We will use simple integer Keys to refer to the robot poses.
 #include <gtsam/inference/Key.h>
 
 // As in OdometryExample.cpp, we use a BetweenFactor to model odometry measurements.
 #include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/slam/PriorFactor.h>
 
 // We add all facors to a Nonlinear Factor Graph, as our factors are nonlinear.
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
@@ -65,19 +70,20 @@ using namespace gtsam;
 // the NoiseModelFactor1.
 #include <gtsam/nonlinear/NonlinearFactor.h>
 
-class UnaryFactor: public NoiseModelFactor1<Pose2> {
+class UnaryFactor: public NoiseModelFactor1<Pose3> {
 
   // The factor will hold a measurement consisting of an (X,Y) location
   // We could this with a Point2 but here we just use two doubles
-  double mx_, my_;
+  double mz_;
 
 public:
   /// shorthand for a smart pointer to a factor
   typedef boost::shared_ptr<UnaryFactor> shared_ptr;
 
   // The constructor requires the variable key, the (X, Y) measurement value, and the noise model
-  UnaryFactor(Key j, double x, double y, const SharedNoiseModel& model):
-    NoiseModelFactor1<Pose2>(model, j), mx_(x), my_(y) {}
+  UnaryFactor(Key j, double z, const SharedNoiseModel& model):
+    NoiseModelFactor1<Pose3>(model, j), mz_(z) {}
+
 
   virtual ~UnaryFactor() {}
 
@@ -85,7 +91,7 @@ public:
   // The first is the 'evaluateError' function. This function implements the desired measurement
   // function, returning a vector of errors when evaluated at the provided variable value. It
   // must also calculate the Jacobians for this measurement function, if requested.
-  Vector evaluateError(const Pose2& q, boost::optional<Matrix&> H = boost::none) const
+  Vector evaluateError(const Pose3& q, boost::optional<Matrix&> H = boost::none) const
   {
     // The measurement function for a GPS-like measurement is simple:
     // error_x = pose.x - measurement.x
@@ -93,8 +99,8 @@ public:
     // Consequently, the Jacobians are:
     // [ derror_x/dx  derror_x/dy  derror_x/dtheta ] = [1 0 0]
     // [ derror_y/dx  derror_y/dy  derror_y/dtheta ] = [0 1 0]
-    if (H) (*H) = (Matrix(2,3) << 1.0,0.0,0.0, 0.0,1.0,0.0).finished();
-    return (Vector(2) << q.x() - mx_, q.y() - my_).finished();
+    if (H) (*H) = (Matrix(1,6) << 0.0,0.0,1.0,0.0,0.0,0.0).finished();
+    return (Vector(1) << q.z() - mz_).finished();
   }
 
   // The second is a 'clone' function that allows the factor to be copied. Under most
@@ -118,25 +124,28 @@ int main(int argc, char** argv) {
 
   // 2a. Add odometry factors
   // For simplicity, we will use the same noise model for each odometry factor
-  noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Sigmas(Vector3(0.2, 0.2, 0.1));
+  noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Variances((Vector(6) << 0.2, 0.2, 0.2, 0.1, 0.1, 0.1).finished());
   // Create odometry (Between) factors between consecutive poses
-  graph.emplace_shared<BetweenFactor<Pose2> >(1, 2, Pose2(2.0, 0.0, 0.0), odometryNoise);
-  graph.emplace_shared<BetweenFactor<Pose2> >(2, 3, Pose2(2.0, 0.0, 0.0), odometryNoise);
+  Rot3 zeroRot3 = Rot3::ypr(0, 0, 0);
+  Point3 zonlyPoint3(0,0,1.1);
+  graph.add(PriorFactor<Pose3>(1, Pose3(Rot3::ypr(0.0, 0.0, 0.0), Point3(0.0, 0.0, 0.0))));
+  graph.emplace_shared<BetweenFactor<Pose3> >(1, 2, Pose3(zeroRot3, zonlyPoint3), odometryNoise);
+  graph.emplace_shared<BetweenFactor<Pose3> >(2, 3, Pose3(zeroRot3, zonlyPoint3), odometryNoise);
 
   // 2b. Add "GPS-like" measurements
   // We will use our custom UnaryFactor for this.
-  noiseModel::Diagonal::shared_ptr unaryNoise = noiseModel::Diagonal::Sigmas(Vector2(0.1, 0.1)); // 10cm std on x,y
-  graph.emplace_shared<UnaryFactor>(1, 0.0, 0.0, unaryNoise);
-  graph.emplace_shared<UnaryFactor>(2, 2.0, 0.0, unaryNoise);
-  graph.emplace_shared<UnaryFactor>(3, 4.0, 0.0, unaryNoise);
+  noiseModel::Diagonal::shared_ptr unaryNoise = noiseModel::Diagonal::Sigmas(Vector1(0.000001)); // 10cm std on x,y
+  graph.emplace_shared<UnaryFactor>(1, 0.0, unaryNoise);
+  graph.emplace_shared<UnaryFactor>(2, 1.0, unaryNoise);
+  graph.emplace_shared<UnaryFactor>(3, 2.0, unaryNoise);
   graph.print("\nFactor Graph:\n"); // print
 
   // 3. Create the data structure to hold the initialEstimate estimate to the solution
   // For illustrative purposes, these have been deliberately set to incorrect values
   Values initialEstimate;
-  initialEstimate.insert(1, Pose2(0.5, 0.0, 0.2));
-  initialEstimate.insert(2, Pose2(2.3, 0.1, -0.2));
-  initialEstimate.insert(3, Pose2(4.1, 0.1, 0.1));
+  initialEstimate.insert(1, Pose3(Rot3::ypr(0.0, 0.0, 0.0), Point3(0.0, 0.0, 0.5)));
+  initialEstimate.insert(2, Pose3(Rot3::ypr(0.0, 0.0, 0.0), Point3(0.0, 0.0, 1.5)));
+  initialEstimate.insert(3, Pose3(Rot3::ypr(0.0, 0.0, 0.0), Point3(0.0, 0.0, 2.5)));
   initialEstimate.print("\nInitial Estimate:\n"); // print
 
   // 4. Optimize using Levenberg-Marquardt optimization. The optimizer
