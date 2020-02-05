78ffb4b560dc5bc374a054f37818218e8d5c0b6e
Anthony Spears <aspears@gatech.edu>
2020-02-05 10:34:59 -0500

    Completed simple pose3 example for DVL

--- DVL_Example/DVL_Example.cpp
+++ DVL_Example/DVL_Example.cpp
@@ -70,53 +70,6 @@ using namespace gtsam;
 // the NoiseModelFactor1.
 #include <gtsam/nonlinear/NonlinearFactor.h>
 
-class UnaryFactor: public NoiseModelFactor1<Pose3> {
-
-  // The factor will hold a measurement consisting of an (X,Y) location
-  // We could this with a Point2 but here we just use two doubles
-  double mz_;
-
-public:
-  /// shorthand for a smart pointer to a factor
-  typedef boost::shared_ptr<UnaryFactor> shared_ptr;
-
-  // The constructor requires the variable key, the (X, Y) measurement value, and the noise model
-  UnaryFactor(Key j, double z, const SharedNoiseModel& model):
-    NoiseModelFactor1<Pose3>(model, j), mz_(z) {}
-
-
-  virtual ~UnaryFactor() {}
-
-  // Using the NoiseModelFactor1 base class there are two functions that must be overridden.
-  // The first is the 'evaluateError' function. This function implements the desired measurement
-  // function, returning a vector of errors when evaluated at the provided variable value. It
-  // must also calculate the Jacobians for this measurement function, if requested.
-  Vector evaluateError(const Pose3& q, boost::optional<Matrix&> H = boost::none) const
-  {
-    // The measurement function for a GPS-like measurement is simple:
-    // error_x = pose.x - measurement.x
-    // error_y = pose.y - measurement.y
-    // Consequently, the Jacobians are:
-    // [ derror_x/dx  derror_x/dy  derror_x/dtheta ] = [1 0 0]
-    // [ derror_y/dx  derror_y/dy  derror_y/dtheta ] = [0 1 0]
-    if (H) (*H) = (Matrix(1,6) << 0.0,0.0,0.0,0.0,0.0,1.0).finished();
-    return (Vector(1) << q.z() - mz_).finished();
-  }
-
-  // The second is a 'clone' function that allows the factor to be copied. Under most
-  // circumstances, the following code that employs the default copy constructor should
-  // work fine.
-  virtual gtsam::NonlinearFactor::shared_ptr clone() const {
-    return boost::static_pointer_cast<gtsam::NonlinearFactor>(
-        gtsam::NonlinearFactor::shared_ptr(new UnaryFactor(*this))); }
-
-  // Additionally, we encourage you the use of unit testing your custom factors,
-  // (as all GTSAM factors are), in which you would need an equals and print, to satisfy the
-  // GTSAM_CONCEPT_TESTABLE_INST(T) defined in Testable.h, but these are not needed below.
-
-}; // UnaryFactor
-
-
 int main(int argc, char** argv) {
 
   //Syntactic sugar to clarify velocity components
@@ -133,9 +86,7 @@ int main(int argc, char** argv) {
   
   // Create odometry (Between) factors between consecutive poses
   Rot3 zeroRot3 = Rot3::ypr(0, 0, 0);
-  Point3 zonlyPoint3(0,0,2);
-  graph.add(PriorFactor<Pose3>(1, Pose3(Rot3::ypr(0.0, 0.0, 0.0), Point3(0.0, 0.0, 0.0)),priorNoise));
-
+  graph.add(PriorFactor<Pose3>(1, Pose3(),priorNoise));
 
   //Add pre-integrated DVL velocity factors
   double dt =0.1; //10Hz
@@ -148,26 +99,15 @@ int main(int argc, char** argv) {
   //Trapezoidal numerical integration
   Point3 preintDVL;
   preintDVL = Point3((DVL_velocities+prev_DVL_velocities)*dt*0.5);
-  
+
+  //Add DVL Factors
   graph.emplace_shared<BetweenFactor<Pose3> >(1, 2, Pose3(zeroRot3, preintDVL), DVLNoise);
   graph.emplace_shared<BetweenFactor<Pose3> >(2, 3, Pose3(zeroRot3, preintDVL), DVLNoise);
 
-  //Add DVL Velocity Constraints
-  graph += VelocityConstraint(1,2,dt);
-  graph += VelocityConstraint(2,3,dt);
-
-
-  // 2b. Add "GPS-like" measurements
-  // We will use our custom UnaryFactor for this.
-  //noiseModel::Diagonal::shared_ptr unaryNoise = noiseModel::Diagonal::Sigmas(Vector1(0.000001)); // 10cm std on x,y
-  //graph.emplace_shared<UnaryFactor>(1, 0.0, unaryNoise);
-  //graph.emplace_shared<UnaryFactor>(2, 1.0, unaryNoise);
-  //graph.emplace_shared<UnaryFactor>(3, 2.0, unaryNoise);
+  //Print Factor Graph
   graph.print("\nFactor Graph:\n"); // print
 
-
-
-  
+ 
   // 3. Create the data structure to hold the initialEstimate estimate to the solution
   // For illustrative purposes, these have been deliberately set to incorrect values
   Values initialEstimate;
